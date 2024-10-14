3694100b670d43c5453ff4819bd21d689f71ea92
Kai Evans <hi@kaievans.co>
2024-10-14 18:33:54 +1100

    Stashig the work

--- zmk-config/build.sh
+++ zmk-config/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pushd /workspaces/zmk/app
+# pushd /workspaces/zmk/app
 
-west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
-cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
+# west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
+# cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
 
-west build -d build/right -p -b nice_nano_v2 -- -DSHIELD="little_wing_right nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
+west build -d build/right -p -b nice_nano_v2 -S zmk-usb-logging -- -DSHIELD="little_wing_right" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
 cp build/right/zephyr/zmk.uf2 /workspaces/zmk-config/build/right.uf2
