78132952acaad041aca6d5377ce872d5bc4b78f3
Kai Evans <hi@kaievans.co>
2024-10-27 09:37:08 +1100

    Making the mouse layers work

--- zmk-config/build.sh
+++ zmk-config/build.sh
@@ -5,5 +5,5 @@ pushd /workspaces/pete-zmk/app
 # west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
 # cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
 
-west build -d build/right -p -b nice_nano_v2 -S zmk-usb-logging -- -DSHIELD="little_wing_right" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
+west build -d build/right -p -b nice_nano_v2 -- -DSHIELD="little_wing_right" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
 cp build/right/zephyr/zmk.uf2 /workspaces/zmk-config/build/right.uf2
