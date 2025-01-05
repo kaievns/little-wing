3e46ccbf5a95eacf9838b818fb00737b249d9bf4
Kai Evans <hi@kaievans.co>
2025-01-05 14:33:28 +1100

    Intergating the nice-view displays back

--- zmk-config/build.sh
+++ zmk-config/build.sh
@@ -2,8 +2,8 @@
 
 pushd /workspaces/pete-zmk/app
 
-west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
+west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
 cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
 
-west build -d build/right -p -b nice_nano_v2 -- -DSHIELD="little_wing_right" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
+west build -d build/right -p -b nice_nano_v2 -- -DSHIELD="little_wing_right nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
 cp build/right/zephyr/zmk.uf2 /workspaces/zmk-config/build/right.uf2
