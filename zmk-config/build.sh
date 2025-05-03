d4f15e6f0a565989354c79cd65d416ecf3db8929
Kai Evans <hi@kaievans.co>
2025-05-03 22:05:33 +1000

    Patching some of the wiring

--- zmk-config/build.sh
+++ zmk-config/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /workspaces/pete-zmk/app
+pushd /workspaces/zmk/app
 
 west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
 cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
