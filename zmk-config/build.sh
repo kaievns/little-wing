f8cbb68b6e529ba5967c4921e20e85bd6f4784c0
Kai Evans <hi@kaievans.co>
2025-05-14 19:49:17 +1000

    Moving the main half to the right side to simplify trackball handling

--- zmk-config/build.sh
+++ zmk-config/build.sh
@@ -2,8 +2,17 @@
 
 pushd /workspaces/zmk/app
 
-west build -d build/left -p -b nice_nano_v2 -- -DSHIELD="little_wing_left nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config" 
+west build -d build/left -p -b nice_nano_v2 -- \
+  -DSHIELD="little_wing_left nice_view" \
+  -DZMK_CONFIG="/workspaces/zmk-config/config" \
+  -DZMK_EXTRA_MODULES="/workspaces/zmk-config"
+
 cp build/left/zephyr/zmk.uf2 /workspaces/zmk-config/build/left.uf2
 
-west build -d build/right -p -b nice_nano_v2 -- -DSHIELD="little_wing_right nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" 
+west build -d build/right -p -b nice_nano_v2 -- \
+  -DSHIELD="little_wing_right nice_view" \
+  -DZMK_CONFIG="/workspaces/zmk-config/config" \
+  -DZMK_EXTRA_MODULES="/workspaces/zmk-config;/workspaces/zmk-config/drivers/pmw3610" \
+  -DKEYMAP_FILE="/workspaces/zmk-config/config/little_wing.keymap;/workspaces/zmk-config/config/little_wing_right.keymap"
+
 cp build/right/zephyr/zmk.uf2 /workspaces/zmk-config/build/right.uf2
